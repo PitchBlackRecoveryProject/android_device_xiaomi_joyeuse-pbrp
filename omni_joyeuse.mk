113a6024cd2c11992cd65df1c7d3548fcba619cc
Manjot Sidhu <manjot.techie@gmail.com>
2020-06-29 14:34:31 +0530

    joyeuse: PB-ify

--- omni_joyeuse.mk
+++ omni_joyeuse.mk
@@ -25,7 +25,7 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
